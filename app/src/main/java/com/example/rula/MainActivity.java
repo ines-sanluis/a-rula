c329fc8d2cc8a705c1aae9069fd925cc5cd936ee
ines-sanluis <sanluis.tfg@gmail.com>
2019-05-15 10:45:06 +0200

    Setting up FireBase

--- app/src/main/java/com/example/rula/MainActivity.java
+++ app/src/main/java/com/example/rula/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
