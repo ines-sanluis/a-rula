f434be30cc3c973386cba85e01a17a182a31eee8
ines-sanluis <sanluis.tfg@gmail.com>
2019-05-20 19:16:01 +0200

    Load main list from Firebase

--- app/src/main/java/com/example/rula/MainActivity.java
+++ app/src/main/java/com/example/rula/MainActivity.java
@@ -1,38 +1,73 @@
 package com.example.rula;
 
-import android.content.Intent;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    //FirebaseFirestore db = FirebaseFirestore.getInstance();
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference myDatabase;
+    private ListView listView;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button upcoming = findViewById(R.id.upcoming);
-        Button donate = findViewById(R.id.donate);
-        upcoming.setOnClickListener(this);
-        donate.setOnClickListener(this);
+
+        myDatabase = FirebaseDatabase.getInstance().getReference();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView = (ListView) findViewById(R.id.trips_list_view);
+
+        TextView textView = new TextView(getApplication().getApplicationContext());
+        textView.setText("Upcoming trips");
+
+        listView.addHeaderView(textView);
+
+        listView.setAdapter(adapter);
+
+        myDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String string = dataSnapshot.getValue(String.class);
+                arrayList.add(string);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.upcoming:
-                Intent myIntent1 = new Intent(MainActivity.this, UpcomingActivity.class);
-                MainActivity.this.startActivity(myIntent1);
-                //Toast.makeText(this, "Upcoming button clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.donate:
-                Intent myIntent2 = new Intent(MainActivity.this, DonateActivity.class);
-                MainActivity.this.startActivity(myIntent2);
-                break;
-        }
-    }
 }
