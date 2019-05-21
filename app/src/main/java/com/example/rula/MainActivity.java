f27cedd9124c1c46d53c5c3366f396cf2e603076
ines-sanluis <sanluis.tfg@gmail.com>
2019-05-21 21:18:33 +0200

    Get key when list element clicked

--- app/src/main/java/com/example/rula/MainActivity.java
+++ app/src/main/java/com/example/rula/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.rula;
 
-import android.content.Context;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference myDatabase;
     private ListView listView;
     private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayList<String> myKeys = new ArrayList<>();
     private ArrayAdapter<String> adapter;
 
     @Override
@@ -39,12 +43,15 @@ public class MainActivity extends AppCompatActivity {
         listView.addHeaderView(textView);
 
         listView.setAdapter(adapter);
+        listView.setClickable(true);
 
         myDatabase.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String key = dataSnapshot.getKey();
                 String string = dataSnapshot.getValue(String.class);
-                arrayList.add(string);
+                arrayList.add("\n"+string+"\n");
+                myKeys.add(key);
                 adapter.notifyDataSetChanged();
             }
 
@@ -66,6 +73,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //String name = listView.getItemAtPosition(position);
+                String key = myKeys.get(position - 1);
+                Toast.makeText(MainActivity.this, key+"", Toast.LENGTH_SHORT).show();
             }
         });
     }
